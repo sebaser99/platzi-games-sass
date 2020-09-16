b1f03a82dbf517afea6c41760f242f6925ef9ca2
Sebastian Rodriguez <sebaser99@gmail.com>
2020-09-16 01:19:11 -0500

    explorando java script

--- java/java.js
+++ java/java.js
@@ -1,7 +1,14 @@
-function load(){
-    const $burguer = document.getElementsByClassName('burguer__icon');
-    $burguer.addEventListener ('click', ()=>{
-        console.log('hola mundo');
+const $burguer = document.getElementsByClassName('burguer__icon')[0];
+const $nav = document.getElementsByClassName('nav--header')[0];
+const $busquedas = document.getElementById('busqueda');
+const $navUl = $nav.querySelector('ul');
+$burguer.addEventListener('click', menuburguer);
+function menuburguer(){
+    $nav.classList.toggle('nav--header');
+    $busquedas.classList.toggle('header__busqueda');
+    $navUl.classList.toggle('is-active');
+    $busquedas.classList.toggle('is-active-busqueda');
+  
 
-    })
+    
 }
