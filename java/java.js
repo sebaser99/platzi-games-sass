28d6854420df250df735a84967d9a31bcc3bf774
Sebastian Rodriguez <sebaser99@gmail.com>
2020-09-15 21:46:57 -0500

    agrego cambios en responsive, de aquí sigo con java

---
+++ java/java.js
@@ -0,0 +1,7 @@
+function load(){
+    const $burguer = document.getElementsByClassName('burguer__icon');
+    $burguer.addEventListener ('click', ()=>{
+        console.log('hola mundo');
+
+    })
+}
