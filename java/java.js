35e8ee393a8e7b5a973e1f51333ba2d9511f6017
Sebastian Rodriguez <sebaser99@gmail.com>
2020-09-17 22:33:25 -0500

    temrino con java

--- java/java.js
+++ java/java.js
@@ -1,10 +1,13 @@
+const ipad = window.matchMedia('screen and (max-width: 992px)');
+const $header = document.getElementsByClassName('header--principal')[0];
+const $nav = $header.querySelector('nav');
 const $burguer = document.getElementsByClassName('burguer__icon')[0];
-const $nav = document.getElementsByClassName('nav--header')[0];
+const $classnav = document.getElementsByClassName('nav--header')[0];
 const $busquedas = document.getElementById('busqueda');
 const $navUl = $nav.querySelector('ul');
-$burguer.addEventListener('click', menuburguer);
+
 function menuburguer(){
-    $nav.classList.toggle('nav--header');
+    $classnav.classList.toggle('nav--header');
     $busquedas.classList.toggle('header__busqueda');
     $navUl.classList.toggle('is-active');
     $busquedas.classList.toggle('is-active-busqueda');
@@ -12,3 +15,33 @@ function menuburguer(){
 
     
 }
+function pantallaGrande(){
+    
+    if ($navUl.classList.contains('is-active')) {
+        $navUl.classList.remove('is-active');
+        $busquedas.classList.remove('is-active-busqueda');
+        $nav.classList.add('nav--header');
+        $busquedas.classList.add('header__busqueda');
+    }
+
+}
+
+ipad.addListener(cambioPantalla);
+function cambioPantalla(tamaño){
+    if(tamaño.matches){
+     
+        $burguer.addEventListener('click', menuburguer);
+
+    }
+    else{
+       pantallaGrande();
+    
+    }
+    
+}
+
+cambioPantalla(ipad);
+
+
+
+
